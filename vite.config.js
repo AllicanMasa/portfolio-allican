c2a8c965e94b1d8cb29be2d5ab4f3b1fbde31593
Allican Masa <128886882+AllicanMasa@users.noreply.github.com>
2025-10-11 17:03:34 +0800

    oks na 'to

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// See https://vitejs.dev/config/ for official Vite configuration documentation
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-allican/'  // <-- your repo name
 })
